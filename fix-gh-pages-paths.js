95b769f11b23006b18f055aca5b9fd78f7ee44c0
Chris Kennedy <chrisken@gmail.com>
2025-08-30 21:16:23 -0400

    Fix GitHub Pages SPA routing for subdirectory deployment
    - Update 404.html to properly handle subdirectory routing
    - Fix routing script in index.html for Expo Router compatibility
    - Ensure proper path preservation for /suicide-safety-planning-app/ base
    - This should resolve the 'This screen doesn't exist' error on GitHub Pages

--- fix-gh-pages-paths.js
+++ fix-gh-pages-paths.js
@@ -28,33 +28,30 @@ function fixPaths() {
     <meta charset="utf-8">
     <title>Suicide Safety Planner</title>
     <script type="text/javascript">
-      // Single Page Apps for GitHub Pages
-      // MIT License
-      // https://github.com/rafgraph/spa-github-pages
-      // This script takes the current url and converts the path and query
-      // string into just a query string, and then redirects the browser
-      // to the new url with only a query string and hash fragment,
-      // e.g. https://www.foo.tld/one/two?a=b&c=d#qwe, becomes
-      // https://www.foo.tld/?/one/two&a=b~and~c=d#qwe
-      // Note: this 404.html file must be at least 512 bytes for it to work
-      // with Internet Explorer (it is currently > 512 bytes)
-
-      // If you're creating a Project Pages site and NOT using a custom domain,
-      // then set pathSegmentsToKeep to 1 (enterprise users may need to set it to > 1).
-      // This way the code will only replace the route part and not the real directory.
-      // This script will NOT work if your site is hosted in a subdirectory,
-      // for example: https://username.github.io/repo-name/
-      // In this case, you would set pathSegmentsToKeep to 1 (or 2, 3, etc).
+      // GitHub Pages SPA routing for subdirectory deployment
+      // For https://username.github.io/repo-name/ deployment
       var pathSegmentsToKeep = 1;
 
       var l = window.location;
-      l.replace(
-        l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') +
-        l.pathname.split('/').slice(0, 1 + pathSegmentsToKeep).join('/') + '/?/' +
-        l.pathname.slice(1).split('/').slice(pathSegmentsToKeep).join('/').replace(/&/g, '~and~') +
-        (l.search ? '&' + l.search.slice(1).replace(/&/g, '~and~') : '') +
-        l.hash
-      );
+      var pathArray = l.pathname.split('/');
+      
+      // For subdirectory deployment, we need to preserve the repo name
+      // and redirect internal routes properly
+      if (pathArray.length > 2) {
+        // We have a route beyond the repo name, redirect to index with hash
+        var repoName = pathArray[1]; // 'suicide-safety-planning-app'
+        var route = pathArray.slice(2).join('/');
+        
+        var redirectUrl = l.protocol + '//' + l.hostname + '/' + repoName + '/?/' + route;
+        if (l.search) {
+          redirectUrl += '&' + l.search.slice(1).replace(/&/g, '~and~');
+        }
+        if (l.hash) {
+          redirectUrl += l.hash;
+        }
+        
+        l.replace(redirectUrl);
+      }
     </script>
   </head>
   <body>
@@ -70,24 +67,18 @@ function fixPaths() {
     
     const routingScript = `
     <script type="text/javascript">
-      // Single Page Apps for GitHub Pages
-      // MIT License
-      // https://github.com/rafgraph/spa-github-pages
-      // This script checks to see if a redirect is present in the query string,
-      // converts it back into the correct url and adds it to the
-      // browser's history using window.history.replaceState(...),
-      // which won't cause the browser to attempt to load the new url.
-      // When the single page app is loaded further down in this file,
-      // the correct url will be waiting in the browser's history for
-      // the single page app to route accordingly.
+      // GitHub Pages SPA routing for Expo Router subdirectory deployment
       (function(l) {
         if (l.search[1] === '/' ) {
           var decoded = l.search.slice(1).split('&').map(function(s) { 
             return s.replace(/~and~/g, '&')
           }).join('?');
-          window.history.replaceState(null, null,
-              l.pathname.slice(0, -1) + decoded + l.hash
-          );
+          
+          // For subdirectory deployment, preserve the base path
+          var basePath = '/suicide-safety-planning-app';
+          var newPath = basePath + decoded;
+          
+          window.history.replaceState(null, null, newPath + l.hash);
         }
       }(window.location))
     </script>`;
